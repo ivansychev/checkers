77f106b074263a314f50b8fcdfb6baf07952dd60
ivansychev <mr.ivan.sychev@gmail.com>
2018-10-09 17:42:37 +0300

    work in progress

--- src/Checkers/logic/game/rules.js
+++ src/Checkers/logic/game/rules.js
@@ -1,4 +1,4 @@
-import { CACHE, PIECES, CELLS } from "../store/state";
+import { CACHE, PIECES, CELLS, CELL_SIDE, HALF_CELLS_SIDE } from "../store/state";
 import * as utils from './utils'
 import { removeSelectedCellAndPiece } from "./game";
 
@@ -14,8 +14,8 @@ export function movePiece(){
             dy =CACHE.clicked.y;
 
         if(legalMove(piece, x, y, dx, dy)){
-            piece.x = 50 * CACHE.clicked.x + 25
-            piece.y = 50 * CACHE.clicked.y + 25
+            piece.x = CELL_SIDE * CACHE.clicked.x + HALF_CELLS_SIDE;
+            piece.y = CELL_SIDE * CACHE.clicked.y + HALF_CELLS_SIDE;
             PIECES[CACHE.selected.y][CACHE.selected.x] = 0;
             PIECES[CACHE.clicked.y][CACHE.clicked.x] = piece;
             removeSelectedCellAndPiece()
@@ -27,28 +27,65 @@ export function movePiece(){
     }
 }
 
+const LEGAL_MOVES = [];
+
 export function legalMove(piece, x, y, dx, dy){
+    //shoudEatPiece(piece, x, y, dx, dy);
+    isMovingLegally(x, y, dx, dy);
+}
+
+export function isMovingLegally(x, y, dx, dy){
     if(CACHE.turn === 'white')
         return (x + 1 === dx || x - 1 === dx) && y - 1 === dy;
     return (x + 1 === dx || x - 1 === dx) && y + 1 === dy;
 }
 
+
+//TODO add cells coordinates to pieces and review your logic
+//TODO simplify this function
+//TODO you would need to analyse every Piece each turn anyway. F.e. when opponent move towards you, you need to analyse which cell is able to hit and wich is able to move. Parsing 64 array is not that expensive anyway
 export function shoudEatPiece(piece, x, y, dx, dy){
-    let opt1, opt2, afterPiece1, afterPiece2;
+
+    let opt1, opt2, aftPiece1Ex, aftPiece2Ex;
     if(CACHE.turn === 'white' && y>1){
         if(x<6) opt1 = PIECES[y-1][x+1]
         if(x>1) opt2 = PIECES[y-1][x-1]
-        if(CACHE.turn !== opt1.side) afterPiece1 = PIECES[y-2][x+2]
-        if(CACHE.turn !== opt2.side) afterPiece2 = PIECES[y-2][x-2]
+        if(opt1 && CACHE.turn !== opt1.side) aftPiece1Ex = utils.isEmptyCell(y-2, x+2)
+        if(opt2 && CACHE.turn !== opt2.side) aftPiece2Ex = utils.isEmptyCell(y-2, x-2)
 
-        //TODO add cells coordinates to pieces and review your logic
+        if(aftPiece1Ex && aftPiece2Ex){
+            return (
+                (x + 2 === dx || x - 2 === dx) && y - 2 === dy
+                || (x - 2 === dx || x - 2 === dx) && y - 2 === dy
+            )
+        }
+        else if(aftPiece1Ex){
+            return (x + 2 === dx || x - 2 === dx) && y - 2 === dy;
+        }else if(aftPiece2Ex){
+            return (x - 2 === dx || x - 2 === dx) && y - 2 === dy;
+        }
     }
+
     if(CACHE.turn === 'black' && y<6){
         if(x<6) opt1 = PIECES[y+1][x+1]
         if(x>1) opt2 = PIECES[y+1][x-1]
-        if(CACHE.turn !== opt1.side) afterPiece1 = PIECES[y+2][x+2]
-        if(CACHE.turn !== opt2.side) afterPiece2 = PIECES[y+2][x-2]
+        if(opt1 && CACHE.turn !== opt1.side) aftPiece1Ex = utils.isEmptyCell(y+2, x+2)
+        if(opt2 && CACHE.turn !== opt2.side) aftPiece2Ex = utils.isEmptyCell(y+2, x-2)
+
+        if(aftPiece1Ex && aftPiece2Ex){
+            return (
+                (x + 2 === dx || x - 2 === dx) && y + 2 === dy
+                || (x - 2 === dx || x - 2 === dx) && y + 2 === dy
+            )
+        }
+        else if(aftPiece1Ex){
+            return (x + 2 === dx || x - 2 === dx) && y + 2 === dy;
+        }else if(aftPiece2Ex){
+            return (x - 2 === dx || x - 2 === dx) && y + 2 === dy;
+        }
 
     }
+
+    return false;
 }
 
